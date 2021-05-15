4b6ceff68c5ba3db304ebdfea4c65e03bda32354
Andrea Zanin <andrea@igloo.ooo>
2021-05-15 17:37:18 +0200

    Integrated with notion api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var fs = require("fs")
 var parse = require("@sole/kindle-clippings-parser").parse
 const cliSelect = require("cli-select")
+const fetch = require("isomorphic-fetch")
+require("dotenv").config()
 
 var fileContents = fs.readFileSync(
     "/Volumes/Kindle/documents/My Clippings.txt",
@@ -9,12 +11,46 @@ var fileContents = fs.readFileSync(
 var parsed = parse(fileContents)
 
 cliSelect({
-    values: parsed.map((item) => item.title),
-}).then(({ id }) => {
+    values: parsed.reverse().map((item) => item.title),
+}).then(async ({ id }) => {
     item = parsed[id]
-    const mdSource = item.highlights.reduce(
-        (acc, { text }) => acc + text + "\n\n",
-        ""
-    )
-    fs.writeFileSync(`./${item.title}.md`, mdSource)
+
+    const pageContent = item.highlights.map(({ text }) => ({
+        object: "block",
+        type: "paragraph",
+        paragraph: {
+            text: [
+                {
+                    type: "text",
+                    text: {
+                        content: text,
+                    },
+                },
+            ],
+        },
+    }))
+
+    const res = await fetch("https://api.notion.com/v1/pages", {
+        method: "POST",
+        headers: {
+            Authorization: "Bearer " + process.env.NOTION_KEY,
+            "Notion-Version": "2021-05-13",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            parent: { database_id: process.env.NOTION_DB },
+            properties: {
+                Name: {
+                    title: [
+                        {
+                            text: {
+                                content: item.title,
+                            },
+                        },
+                    ],
+                },
+            },
+            children: pageContent,
+        }),
+    }).then((res) => res.json())
 })
